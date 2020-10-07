264daf60005d051c457887d972ea3e6b0df0107b
Daniel Njoroge <njoroge.consultant@gmail.com>
2020-10-07 16:08:26 -0400

    Add the files

--- core/src/main/java/com/editable/components/core/listeners/EditableComponentsListener.java
+++ core/src/main/java/com/editable/components/core/listeners/EditableComponentsListener.java
@@ -3,8 +3,8 @@ package com.editable.components.core.listeners;
 import com.editable.components.core.jcr.JcrUtil;
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.jcr.api.SlingRepository;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -21,6 +21,9 @@ import javax.jcr.observation.Event;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.EventListener;
 import javax.jcr.observation.ObservationManager;
+
+import static org.apache.jackrabbit.JcrConstants.JCR_CONTENT;
+import static org.apache.jackrabbit.JcrConstants.NT_UNSTRUCTURED;
 
 /**
  * Event Listener that listens to Editable component event
@@ -41,17 +44,17 @@ public class EditableComponentsListener implements EventListener {
     @Reference
     private SlingRepository slingRepository;
 
-    private static final String NT_UNSTRUCTURED = "nt:unstructured";
-
     private Session session;
 
     @Activate
     protected void activate(ComponentContext componentContext) {
         try {
             session = JcrUtil.getSession(slingRepository);
-            final String[] types = {NT_UNSTRUCTURED};
+            final String[] types = {
+                    NT_UNSTRUCTURED
+            };
             final ObservationManager observationManager = session.getWorkspace().getObservationManager();
-            observationManager.addEventListener(this, Event.PROPERTY_CHANGED , EDITABLE_COMPONENTS_CONTENT_PATH,
+            observationManager.addEventListener(this, Event.PROPERTY_CHANGED, EDITABLE_COMPONENTS_CONTENT_PATH,
                     true, null, types, false);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -60,9 +63,9 @@ public class EditableComponentsListener implements EventListener {
 
     @Deactivate
     protected void deactivate() throws RepositoryException {
-        if(session != null) {
+        if (session != null) {
             ObservationManager observationManager = session.getWorkspace().getObservationManager();
-            if (observationManager != null){
+            if (observationManager != null) {
                 observationManager.removeEventListener(this);
             }
             session.logout();
@@ -72,27 +75,30 @@ public class EditableComponentsListener implements EventListener {
     @Override
     public void onEvent(EventIterator events) {
         try {
-            while(events.hasNext()) {
+            while (events.hasNext()) {
                 Event event = events.nextEvent();
                 final String eventPath = event.getPath();
-                // check if jcr:lastModified for only JCR modification to the component node
-            //    if (StringUtils.equals(StringUtils.substringAfterLast(eventPath, "/"), NameConstants.PN_LAST_MOD)) {
-                    // Current event absolute path
-                    final String eventJcrContentPath = event.getIdentifier();
-                    final ResourceResolver resourceResolver = JcrUtil.getResourceResolver(resolverFactory);
-                    final Resource componentResource = resourceResolver.getResource(eventJcrContentPath);
-                    if (componentResource != null) {
-                       final String resourceType = componentResource.getResourceType();
-                        // verify is Editable Component modification
-                        if (StringUtils.contains(resourceType, EDITABLE_COMPONENT_RESOURCE_SUPER_TYPE)) {
-                            // add jcr:content/root
-                            String componentsStructureOrigin = componentResource.getValueMap().get("fragmentVariationPath", "")+"/jcr:content/root";
-                            // Localize the components structure view
-                            JcrUtil.copyNodes(componentsStructureOrigin,eventJcrContentPath,resolverFactory);
-                        }
+                // Current event absolute path
+                final String eventJcrContentPath = StringUtils.substringBeforeLast(eventPath, "/");
+                final Resource componentResource = JcrUtil.getResourceResolver(resolverFactory).getResource(eventJcrContentPath);
+                if (componentResource != null) {
+                    final String resourceType = componentResource.getResourceType();
+                    // verify is Editable Component modification
+                    if (StringUtils.contains(resourceType, EDITABLE_COMPONENT_RESOURCE_SUPER_TYPE)) {
+
+                        ValueMap properties = componentResource.getValueMap();
+                        // add jcr:content/root
+                        final String componentsStructureOrigin = properties.get("fragmentVariationPath", "") + "/" + JCR_CONTENT + "/root";
+                        final boolean isRefreshComponents = properties.get("refreshComponents", false);
+
+                        // Localize the components structure view
+                        JcrUtil.copyNodes(componentsStructureOrigin,
+                                eventJcrContentPath,
+                                resolverFactory,
+                                isRefreshComponents);
                     }
                 }
-            //}
+            }
         } catch (Exception e) {
             log.error("Exception occurred", e);
         }
